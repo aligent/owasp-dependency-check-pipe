f750bb2f89db80679c61ffaa4df269dc39b7e93f
John Smith <john.smith@aligent.com.au>
2021-08-12 09:29:36 +0930

    Add default CVSS failure level

--- pipe/pipe.sh
+++ pipe/pipe.sh
@@ -5,16 +5,16 @@ source "$(dirname "$0")/common.sh"
 
 
 validate() {
-     CVSS_FAIL_LEVEL=${CVSS_FAIL_LEVEL:=false}
+     CVSS_FAIL_LEVEL=${CVSS_FAIL_LEVEL:=1}
 }
 
 run_owasp_checks() {
      OWASP_PATH="/usr/share/dependency-check/bin/dependency-check.sh"
 
      if [[ -z "${SUPPRESSION_FILE_PATH}" ]]; then
-          ${OWASP_PATH} --format=JUNIT --project ${BITBUCKET_REPO_FULL_NAME} --enableExperimental --out ./test-results/ -s ${SCAN_PATH} --failOnCVSS ${CVSS_FAIL_LEVEL}
+          ${OWASP_PATH} --format JUNIT --project ${BITBUCKET_REPO_FULL_NAME} --enableExperimental --out ./test-results/ -s ${SCAN_PATH} --junitFailOnCVSS ${CVSS_FAIL_LEVEL} --failOnCVSS ${CVSS_FAIL_LEVEL}
      else
-          ${OWASP_PATH} --format=JUNIT --project ${BITBUCKET_REPO_FULL_NAME} --enableExperimental --out ./test-results/ -s ${SCAN_PATH} --failOnCVSS ${CVSS_FAIL_LEVEL} --suppression ${SUPPRESSION_FILE_PATH}
+          ${OWASP_PATH} --format JUNIT --project ${BITBUCKET_REPO_FULL_NAME} --enableExperimental --out ./test-results/ -s ${SCAN_PATH} --junitFailOnCVSS ${CVSS_FAIL_LEVEL} --failOnCVSS ${CVSS_FAIL_LEVEL} --suppression ${SUPPRESSION_FILE_PATH}
      fi
 
 }
