a40306e23ac486721dd5eb229a3509483444385e
John Smith <john.smith@aligent.com.au>
2021-08-11 11:33:04 +0930

    Correct path for test output

--- pipe/pipe.sh
+++ pipe/pipe.sh
@@ -8,7 +8,7 @@ run_owasp_checks() {
      OWASP_PATH="/usr/share/dependency-check/bin/dependency-check.sh"
      REPORT_PATH=/var/tmp/dependency-check-report.html
 
-     ${OWASP_PATH} --prettyPrint --project ${BITBUCKET_REPO_FULL_NAME} --enableExperimental --out /test-results -s ${SCAN_PATH}
+     ${OWASP_PATH} --prettyPrint --project ${BITBUCKET_REPO_FULL_NAME} --enableExperimental --out /test-results/ -s ${SCAN_PATH}
 }
 
 run_owasp_checks
