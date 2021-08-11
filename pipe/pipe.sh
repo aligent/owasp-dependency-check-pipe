501df80641aa39f3d162e2e604d08d435ff23510
John Smith <john.smith@aligent.com.au>
2021-08-11 19:16:51 +0930

    Set output format to junit

--- pipe/pipe.sh
+++ pipe/pipe.sh
@@ -1,10 +1,11 @@
 #!/usr/bin/env bash
+set -e
 
 source "$(dirname "$0")/common.sh"
 
 run_owasp_checks() {
      OWASP_PATH="/usr/share/dependency-check/bin/dependency-check.sh"
-     ${OWASP_PATH} --format=JSON --prettyPrint --project ${BITBUCKET_REPO_FULL_NAME} --enableExperimental --out ./test-results/ -s ${SCAN_PATH} --failOnCVSS ${CVSS_FAIL_LEVEL}
+     ${OWASP_PATH} --format=JUNIT --project ${BITBUCKET_REPO_FULL_NAME} --enableExperimental --out ./test-results/ -s ${SCAN_PATH} --failOnCVSS ${CVSS_FAIL_LEVEL}
 
      RESULT=$?
      echo "Result:"
