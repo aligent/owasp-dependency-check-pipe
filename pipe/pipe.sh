56c5695c00a872fd86ebbf5d5464201d288ca9c6
John Smith <john.smith@aligent.com.au>
2021-08-11 19:18:35 +0930

    Remove superfluous OWASP result test

--- pipe/pipe.sh
+++ pipe/pipe.sh
@@ -6,14 +6,6 @@ source "$(dirname "$0")/common.sh"
 run_owasp_checks() {
      OWASP_PATH="/usr/share/dependency-check/bin/dependency-check.sh"
      ${OWASP_PATH} --format=JUNIT --project ${BITBUCKET_REPO_FULL_NAME} --enableExperimental --out ./test-results/ -s ${SCAN_PATH} --failOnCVSS ${CVSS_FAIL_LEVEL}
-
-     RESULT=$?
-     echo "Result:"
-     echo $RESULT
-     if [ $RESULT -ne 0 ]; then
-          fail "Build did not pass OWASP security checks. Please see test output."
-     fi
-
 }
 
 run_owasp_checks
