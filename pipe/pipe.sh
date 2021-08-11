ca473705a793404c233ac85a95dc5086304b97a4
John Smith <john.smith@aligent.com.au>
2021-08-11 19:02:57 +0930

    Failure error handling

--- pipe/pipe.sh
+++ pipe/pipe.sh
@@ -1,14 +1,18 @@
 #!/usr/bin/env bash
-#
-set -e
 
 source "$(dirname "$0")/common.sh"
 
 run_owasp_checks() {
      OWASP_PATH="/usr/share/dependency-check/bin/dependency-check.sh"
-     REPORT_PATH=/var/tmp/dependency-check-report.html
+     ${OWASP_PATH} --format=JSON --prettyPrint --project ${BITBUCKET_REPO_FULL_NAME} --enableExperimental --out ./test-results/ -s ${SCAN_PATH} --failOnCVSS ${CVSS_FAIL_LEVEL}
 
-     ${OWASP_PATH} --prettyPrint --project ${BITBUCKET_REPO_FULL_NAME} --enableExperimental --out /test-results/ -s ${SCAN_PATH}
+     RESULT=$?
+     echo "Result:"
+     echo $RESULT
+     if [ $RESULT -ne 0 ]; then
+          fail "Build did not pass OWASP security checks. Please see test output."
+     fi
+
 }
 
 run_owasp_checks
