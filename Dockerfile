372a9c858c51bbd2f445fbccee8aa5281870ebe0
John Smith <john.smith@aligent.com.au>
2022-01-13 16:15:23 +1030

    Add code insights support

--- Dockerfile
+++ Dockerfile
@@ -6,8 +6,21 @@ RUN apk add wget bash
 RUN cd / && wget -P / https://bitbucket.org/bitbucketpipelines/bitbucket-pipes-toolkit-bash/raw/0.4.0/common.sh
 RUN chmod a+x /*.sh
 
+COPY ./code-insights /usr/bin/code-insights
+# Install python/pip
+ENV PYTHONUNBUFFERED=1
+RUN apk add --update --no-cache python3
+RUN python3 -m ensurepip
+RUN pip3 install --no-cache --upgrade pip setuptools
+RUN pip3 install pipenv
+RUN python3 --version
+RUN chown 1000 /usr/bin/code-insights
+
 # https://github.com/jeremylong/DependencyCheck/blob/2d5fbd9719ddd55a59aea8c234c11e43eaafe26d/Dockerfile#L50
 USER 1000
+
+RUN cd /usr/bin/code-insights && pipenv install
+RUN cd /usr/bin/code-insights && pipenv run code-insights
 
 RUN /usr/share/dependency-check/bin/dependency-check.sh --updateonly
 
---
+++ code-insights/code-insights.py
@@ -0,0 +1,92 @@
+import os
+import argparse
+import requests
+import uuid
+
+BITBUCKET_WORKSPACE = os.getenv('BITBUCKET_WORKSPACE')
+BITBUCKET_REPO_SLUG = os.getenv('BITBUCKET_REPO_SLUG')
+BITBUCKET_COMMIT = os.getenv('BITBUCKET_COMMIT')
+BITBUCKET_PIPELINE_UUID = os.getenv('BITBUCKET_PIPELINE_UUID')
+BITBUCKET_STEP_UUID = os.getenv('BITBUCKET_STEP_UUID')
+PROXIES = {"http": 'http://localhost:29418'}
+
+def create_report(title, details, report_type, report_id, reporter, result, data):
+    url=f"http://api.bitbucket.org/2.0/repositories/{BITBUCKET_WORKSPACE}/{BITBUCKET_REPO_SLUG}/commit/{BITBUCKET_COMMIT}/reports/{reporter}-{report_id}"
+    body = { 
+            "title": details,
+            "details": title,
+            "report_type": report_type,
+            "reporter": reporter,
+            "link": f"https://bitbucket.org/{BITBUCKET_WORKSPACE}/{BITBUCKET_REPO_SLUG}/addon/pipelines/home#!/results/{BITBUCKET_PIPELINE_UUID}/steps/{BITBUCKET_STEP_UUID}",
+            "result": result,
+            "data": data
+            }
+    r = requests.put(url=url, json=body, proxies=PROXIES)
+    if not r.ok:
+        print(f"Failed to create report {title}")
+        print(r.text)
+
+def create_annotation(title, summary, severity, path, line, reporter, report_id, annotation_type, annotation_id):
+    url=f"http://api.bitbucket.org/2.0/repositories/{BITBUCKET_WORKSPACE}/{BITBUCKET_REPO_SLUG}/commit/{BITBUCKET_COMMIT}/reports/{reporter}-{report_id}/annotations/{reporter}-{annotation_id}"
+    body = { 
+            "title": title,
+            "annotation_type": annotation_type,
+            "summary": summary,
+            "severity": severity,
+            "path": path,
+            "line": line
+            }
+    r = requests.put(url=url, json=body, proxies=PROXIES)
+    if not r.ok:
+        print(f"Failed to create annotation {title}")
+        print(r.text)
+
+def read_results_from_file(file):
+    from junitparser import JUnitXml
+
+    results = []
+    xml = JUnitXml.fromfile(file)
+    if not xml.failures: return []
+    for suite in xml:
+        # handle suites
+        if suite.failures == 0: continue
+        for case in suite:
+            results.append(case.result)
+
+    return results
+
+def build_report_data(result):
+    report_data = [
+            {
+                "title": 'Failures',
+                "type": 'NUMBER',
+                "value": len(result)
+                }
+            ]
+
+    return report_data
+
+parser = argparse.ArgumentParser(description='Interface with the Bitbucket Code Insights API')
+subparsers = parser.add_subparsers(dest="cmd")
+create_parser = subparsers.add_parser('create-report')
+
+create_parser.add_argument('title', type=str)
+create_parser.add_argument('details', type=str)
+create_parser.add_argument('report_type', type=str)
+create_parser.add_argument('reporter', type=str)
+create_parser.add_argument('file', type=str)
+
+args = parser.parse_args()
+
+
+if args.cmd == 'create-report':
+    print(f"Creating Report")
+    results = read_results_from_file(args.file)
+    
+    create_report(args.title,
+            args.details,
+            args.report_type,
+            str(uuid.uuid4()),
+            args.reporter,
+            "FAILED" if len(results) else "PASSED",
+            build_report_data(results))
